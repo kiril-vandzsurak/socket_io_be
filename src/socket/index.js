ade6294efd4a90c78971c34990de4bdb72300de1
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-15 16:05:57 +0100

    adds newUsers adding

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,4 +2,14 @@ let onlineUsers = [];
 
 export const newConnectionHandler = (newClient) => {
   newClient.emit("welcome", { message: `Hello ${newClient.id}` });
+
+  newClient.on("setUsername", (payload) => {
+    console.log(payload);
+
+    onlineUsers.push({ username: payload.username, socketId: newClient.id });
+
+    newClient.emit("loggedIn", onlineUsers);
+
+    newClient.broadcast.emit("updateOnlineUsersList", onlineUsers);
+  });
 };
